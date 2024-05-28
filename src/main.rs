1cc95f391517b5b9fdb299bf36a841d3677b96d0
Eric Gauderman <pixelprizm@gmail.com>
2024-05-27 19:53:27 -0700

    fps_clock

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use fps_clock::*;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    let mut fps_clock = FpsClock::new(60);
+    loop {
+        let i = fps_clock.tick();
+        print!("\rTime since last tick (in secs): {:?}", i / 1000000000.);
+        std::io::stdout().flush().unwrap();
+    }
 }
