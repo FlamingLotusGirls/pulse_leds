37eaae0cd961ded720261f4ffd56668d94aaf12e
Pixelprizm <pixelprizm@gmail.com>
2024-05-29 18:32:15 -0700

    install script

--- install/raspberry_pi_5.sh
+++ install/raspberry_pi_5.sh
@@ -1,1 +1,5 @@
 sudo nmcli connection modify "Wired connection 1" ipv4.method manual ipv4.addr 169.254.5.49/24
+
+# echo "Add the following to /etc/rc.local (ABOVE the \"exit 0\"):"
+# echo "cd ~/code/FlamingLotusGirls/pulse_leds"
+# echo "cargo run &"
